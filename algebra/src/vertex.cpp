7e94585aa3f04bc850334f05b848e60bc6c2414d
bonohub13 <ken.saito.0813@gmail.com>
2020-08-10 02:08:02 +0900

    added feature map

--- algebra/src/vertex.cpp
+++ algebra/src/vertex.cpp
@@ -3,3 +3,11 @@
 #include "vertex_int.cpp"
 #include "vertex_double.cpp"
 #include "vertex_float.cpp"
+
+/* ----- Features to add ---------------
+* Constructors that takes in different types of Vertex and converts them into a different Vertex
+    * ex1) algebra::Vertex<double>(const Vertex<int> &vertex);
+    * ex2) algebra::Vertex<float>(const std::vector<double> &vertex);
+* Make the same for setters for each Vertex
+    * ex) algebra::Vertex<double>::set_vertex(const std::vector<float> &vertex);
+*/
