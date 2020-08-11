933d546ab0df2d2608981b4320cc880752af643f
bonohub13 <ken.saito.0813@gmail.com>
2020-08-12 01:07:27 +0900

    modified matrix_int.hpp

--- algebra/include/algebra/matrix/matrix_int.hpp
+++ algebra/include/algebra/matrix/matrix_int.hpp
@@ -9,14 +9,14 @@ namespace algebra
     {
     public:
         Matrix(const std::vector<std::vector<int>> &matrix);
-        Matrix(const unsigned int &hor_size, const unsigned int &ver_size);
+        Matrix(const unsigned int &hor_size, const unsigned int &ver_size); // (const unsigned int &horizontal size, const unsigned int &vertical size)
     private: // variables
         std::vector<std::vector<int>> _matrix;
     public: // setters and getters
         std::vector<std::vector<int>> get_matrix() const;
         std::array<std::size_t, 2> size() const;
         std::vector<std::vector<int>> set_matrix(const std::vector<std::vector<int>> &matrix);
-    public:
+    public: // operators
         friend std::ostream& operator<<(std::ostream &os, const Matrix<int> &M);
     };
     std::ostream& operator<<(std::ostream &os, const Matrix<int> &M);
