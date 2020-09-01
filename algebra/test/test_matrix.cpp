16c818aa343a273b05f5e657302bb6b390fba8a9
bonohub13 <ken.saito.0813@gmail.com>
2020-09-02 07:09:58 +0900

    updated test_matrix

--- algebra/test/test_matrix.cpp
+++ algebra/test/test_matrix.cpp
@@ -3,14 +3,20 @@
 #include "algebra/matrix/matrix.hpp"
 
 template<typename T1, typename T2>
+void test_adder(algebra::Matrix<T1> A, algebra::Matrix<T2> B)
+{
+    std::cout << "A + B = " << A + B << std::endl;
+}
+
+template<typename T1, typename T2>
 void test_scala(T1 x, algebra::Matrix<T2> A)
 {
-    std::cout << x << " * A = " << x * A << std::endl;
+    std::cout << x << " * " << A << " = " << x * A << std::endl;
 }
 
 int main()
 {
-    algebra::Matrix<int> A(3, 3);
+    algebra::Matrix<int> A(2, 2);
     algebra::Matrix<int> B({{1, 0}, {0, 1}});
 
     std::cout << "A: " << A << std::endl;
@@ -18,5 +24,6 @@ int main()
     std::cout << "B: " << B << std::endl;
     std::cout << "size: (" << B.size()[0] << ", " << B.size()[1] << ")" << std::endl;
 
-    test_scala(12.0, A);
+    test_adder(A, B);
+    test_adder(B, B);
 }
